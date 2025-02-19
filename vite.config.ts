c15336cc65e64f500e9919f3a6ce7297d7f90363
Makhmudov Khikmatilla <k.makhmudov@yt.uz>
2025-02-19 14:25:59 +0500

    add download excel any page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import federation from "@originjs/vite-plugin-federation";
+import path from "path";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    svgr({ svgrOptions: { icon: true } }),
+    react(),
+    federation({
+      name: "app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Button": "./src/Button",
+      },
+      shared: ["react", "react-dom"],
+      remotes: {
+        app2: 'http://localhost:5001/assets/remoteEntry.js',
+      },
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        // This is where you would configure the output similar to webpack's output settings
+        format: 'esm',  // Module format
+        dir: 'dist',    // Output directory
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  }
+});
